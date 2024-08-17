890c3f0f06a6ff1fcd9f84e50a7468079fd67223
AzizNadirov <eziznadirov@gmail.com>
2024-08-17 20:32:01 +0400

    feat: implemented movie factory

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
 
-from src.pytemplate.domain.models import Movie
+from src.pytemplate.domain.models import Movie, movie_factory
 
 
 class TestMovie(TestCase):
     def test_movie(self):
         movie = Movie(name="The Godfather", customer_age="18")
+        self.assertEqual(movie.name, "The Godfather")
+        self.assertEqual(movie.customer_age, "18")
+
+    def test_movie_factory(self):
+        movie = movie_factory(name="The Godfather", customer_age="18")
+        self.assertIsInstance(movie, Movie)
         self.assertEqual(movie.name, "The Godfather")
         self.assertEqual(movie.customer_age, "18")
 
